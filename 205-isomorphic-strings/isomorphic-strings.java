83f2b4ea815cbc1290bb4140a96135189fe13b65
junhanzhang1997 <46697249+junhanzhang1997@users.noreply.github.com>
2024-01-20 22:54:38 -0800

    Time: 23 ms (14.37%) | Memory: 42.6 MB (23.48%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,40 +1,20 @@
-/*class Solution {
+class Solution {
     public boolean isIsomorphic(String s, String t) {
         int l=s.length();
         HashMap<Character,Character> map=new HashMap<>();
+        HashMap<Character,Character> map2=new HashMap<>();
         for(int i=0;i<l;i++){
             char ss=s.charAt(i);
             char tt=t.charAt(i);
             if(map.containsKey(ss)&&map.get(ss)!=tt||
-            map.containsKey(tt)&&map.get(tt)!=ss){
+            map2.containsKey(tt)&&map2.get(tt)!=ss){
                 return false;
             
             }
             map.put(ss,tt);
-            map.put(tt,ss);
+            map2.put(tt,ss);
         }
         return true;
     }
 }
-*/
-class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> map = new HashMap<>();
-        Set<Character> set = new HashSet<>();
-        int n = s.length() - 1;
-        
-        while (n >= 0) {
-            if (map.containsKey(s.charAt(n)) && map.get(s.charAt(n)) != t.charAt(n)) {
-                return false;
-            }
-            if (!map.containsKey(s.charAt(n)) && set.contains(t.charAt(n))) {
-                return false;
-            }
-            set.add(t.charAt(n));
-            map.put(s.charAt(n), t.charAt(n));
-            n--;
-        }
-        
-        return true;
-    }
-}
+
